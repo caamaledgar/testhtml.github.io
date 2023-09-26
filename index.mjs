28b622bf862e76e16b69db07bdda3d9eebb48259
Edgar David Caamal Dzulu <caamal.edgar@gmail.com>
2023-09-26 07:27:17 -0600

    Add files via upload

---
+++ index.mjs
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hola CONALEP 2023!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/15-07-14-Campeche-Kathedrale-RalfR-WMA_0750.jpg/1920px-15-07-14-Campeche-Kathedrale-RalfR-WMA_0750.jpg" />
+ 
+  </div>
+`;
